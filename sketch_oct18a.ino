de8704af72e4587d67c92bb3b48c4621ba869182
kapipara <kapipara_pcmail@yahoo.co.jp>
2017-10-18 20:26:58 +0900

    stepping motor test programs
    ステッピングモータのテストプログラム．
    動きません．
    http://tyokota.hateblo.jp/entry/2017/06/16/191852
    http://deviceplus.jp/hobby/entry037/

---
+++ sketch_oct18a.ino
@@ -0,0 +1,46 @@
+#include <SPI.h>
+
+#define PIN_BUSY 9
+#define PIN_SDO 12 /* SPIデータ出力 */
+#define PIN_SDI 11 /* SPIデータ入力 */
+#define PIN_SS 8
+
+void L6470_send(unsigned char sendData);
+void L6470_resetDevice(); 
+void L6470_constRun();
+
+void setup(){
+  SPI.begin();
+  SPI.setBitOrder( MSBFIRST );
+  SPI.setDataMode( SPI_MODE3 );
+}
+
+void loop(){
+   L6470_constRun();
+}
+
+void L6470_resetDevice(){
+  /* 命令実行中にリセットするのを避けるため空命令を4回くらい送っとく */
+  /* 0x00はNOP(Nothing is performed)命令らしい */
+  L6470_send(0x00);
+  L6470_send(0x00);
+  L6470_send(0x00);
+  L6470_send(0x00);
+  /* Reset信号 */
+  L6470_send(0xc0);
+}
+
+void L6470_send(unsigned char sendData){
+  while(!digitalRead(PIN_BUSY)){ }
+  digitalWrite(PIN_SS,LOW);
+  SPI.transfer(sendData);
+  digitalWrite(PIN_SS,HIGH);
+}
+
+void L6470_constRun(){
+  L6470_send(0x51);
+  L6470_send(0x0a);
+  L6470_send(0x0a);
+  L6470_send(0x0a);
+}
+
